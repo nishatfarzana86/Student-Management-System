b16f7be41c304ed045931036b62048cf6c435e93
Nishat Farzana <59667391+nishatfarzana86@users.noreply.github.com>
2021-03-31 20:22:43 +0600

    Add files via upload

---
+++ storage/framework/views/9cf630ccb657cf11bd3920814766a9670c0297f2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Page Name - <?php echo $__env->yieldContent('title'); ?></title>
+    </head>
+    <body>
+        
+
+        <div class="content">
+            <?php echo $__env->yieldContent('content'); ?>
+        </div>
+
+        
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\SMS\resources\views/layout.blade.php ENDPATH**/ ?>
